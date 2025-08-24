a952f310c06e7d91c1768f23c7f8c1cbde5b62f5
kari-13 <varunmshakthi2013@outlook.com>
2025-08-24 21:45:26 +0530

    Add files via upload

---
+++ mathguru.py
@@ -0,0 +1,14 @@
+import json 
+with open('math.json','r') as file:
+    datab = json.load(file)
+
+while true:
+    a=input("enter a math theorem to find it's explantion :")
+
+    if a in datab:
+        print(datab[a])
+
+    else : print("theorem not found")
+
+    if a.lower() == 'exit':
+        break
